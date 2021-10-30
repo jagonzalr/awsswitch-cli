ff5bcc4e23fb6ae2d57de0986586dbe686592473
José Antonio González Rodríguez <jagonzalr@users.noreply.github.com>
2021-10-30 13:09:01 +0100

    Setup of project (#1)
    * Add initial setup of project
    
    * Add .prettierrc

---
+++ src/cli.js
@@ -0,0 +1,48 @@
+import { Command } from "commander/esm.mjs";
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+const program = new Command();
+
+program
+  .argument("current", "Current default profile")
+  .argument("new", "New default profile")
+  .description(
+    "Updates current default AWS profile saved in .aws config/credentials files"
+  )
+  .action(run);
+
+export async function cli() {
+  await program.parseAsync();
+}
+
+async function run(currentProfile, newProfile) {
+  const homeDir = os.homedir();
+
+  const awsConfigFile = ".aws/config";
+  const awsConfigPath = path.resolve(homeDir, awsConfigFile);
+  const awsConfigContent = fs.readFileSync(awsConfigPath, "utf-8");
+
+  const awsCredentialsFile = ".aws/credentials";
+  const awsCredentialsPath = path.resolve(homeDir, awsCredentialsFile);
+  const awsCredentialsContent = fs.readFileSync(awsCredentialsPath, "utf-8");
+
+  if (!fileHasNewProfile(awsConfigContent, newProfile)) {
+    console.error("No profile in .aws/config found with name", newProfile);
+    process.exit(1);
+  }
+
+  if (!fileHasNewProfile(awsCredentialsContent, newProfile, false)) {
+    console.error("No profile in .aws/credentials found with name", newProfile);
+    process.exit(1);
+  }
+
+  process.exit(0);
+}
+
+function fileHasNewProfile(fileContent, newProfile, isConfig = true) {
+  return isConfig
+    ? fileContent.includes(`[profile ${newProfile}]`)
+    : fileContent.includes(`[${newProfile}]`);
+}
