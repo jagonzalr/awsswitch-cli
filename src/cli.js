4f9327aa7342edd8fb3df803ede723adea643ee8
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2021-10-30 13:35:53 +0100

    Update aws files with new profile name

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,6 @@
 import { Command } from 'commander/esm.mjs'
-import fs from 'fs'
-import os from 'os'
-import path from 'path'
 
-import { fileHasNewProfile } from './utils'
+import { updateFileWithNewProfile } from './utils'
 
 const program = new Command()
 
@@ -20,25 +17,7 @@ export async function cli() {
 }
 
 async function run(currentProfile, newProfile) {
-  const homeDir = os.homedir()
-
-  const awsConfigFile = '.aws/config'
-  const awsConfigPath = path.resolve(homeDir, awsConfigFile)
-  const awsConfigContent = fs.readFileSync(awsConfigPath, 'utf-8')
-
-  const awsCredentialsFile = '.aws/credentials'
-  const awsCredentialsPath = path.resolve(homeDir, awsCredentialsFile)
-  const awsCredentialsContent = fs.readFileSync(awsCredentialsPath, 'utf-8')
-
-  if (!fileHasNewProfile(awsConfigContent, newProfile)) {
-    console.error('No profile in .aws/config found with name', newProfile)
-    process.exit(1)
-  }
-
-  if (!fileHasNewProfile(awsCredentialsContent, newProfile, false)) {
-    console.error('No profile in .aws/credentials found with name', newProfile)
-    process.exit(1)
-  }
-
+  updateFileWithNewProfile(currentProfile, newProfile)
+  updateFileWithNewProfile(currentProfile, newProfile, false)
   process.exit(0)
 }
